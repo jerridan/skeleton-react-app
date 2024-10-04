66b92eaf8f32ea29b29a6aa0d14e04a3d14f2e5a
Jerridan Quiring <jerridan@gmail.com>
2024-10-04 16:50:38 -0400

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx', // You can switch to `.jsx` if not using TypeScript
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,  // Matches .js, .jsx, .ts, and .tsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'swc-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+    port: 3000,
+  },
+  mode: 'development',
+};
